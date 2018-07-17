a4a5cb89d030345d0972eb042cd6352c5e7c8094
Rayman Rosevear <ray@mushroomcloud.co.za>
2018-07-17 16:35:13 +0200

    Import Foundation in LGCentralManager.h

--- LGBluetooth/LGBluetooth.h
+++ LGBluetooth/LGBluetooth.h
@@ -20,6 +20,8 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#import <Foundation/Foundation.h>
+
 #import "LGCentralManager.h"
 #import "LGPeripheral.h"
 #import "LGService.h"
